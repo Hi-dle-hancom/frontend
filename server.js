7893e62cd33472af75f2f5ed7549545f3f918c6c
DoSeon-Lee <doseon1226@gmail.com>
2025-04-02 22:01:06 +0900

    two upload

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 정적 파일 제공
-app.use(express.static(path.join(__dirname, "public")));
+// 정적 파일 제공 - 현재 디렉토리에서 직접 제공
+app.use(express.static(__dirname));
 
 // 메인 라우트
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // 서버 시작
